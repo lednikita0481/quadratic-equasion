5f67cb9fa0d8abfdf90d5a7f6cab44caadb87938
lednikita0481 <lednikita0481@gmail.com>
2022-08-26 13:31:36 +0300

    доработка программы, unit-тестов
    Немного переделал систему ввода режима работы, немного структуры, добавил unit-тесты из файла

--- main.cpp
+++ main.cpp
@@ -4,8 +4,9 @@
 #include <math.h>
 #include <assert.h>
 #include "main.h"
+#define TESTS_AMOUNT 11
 
-const double eps = 0.00000001;
+const double eps = 1e-7;
 
 enum Roots
 {
@@ -17,6 +18,7 @@ enum Roots
     TWO_ROOTS,
     INF_ROOTS
 };
+
 
 struct SquareEq
 {
@@ -34,14 +36,11 @@ struct SquareEq
 
 int main(void)
 {
-    printf("%d\n", NAN == NAN);
-    int working_mode = 3; // 1 - standart input for coeffs, 2 - file input, gets value in work_regime_choice()
-
-    struct SquareEq EqParametrs;
+    Working_Mode working_mode = NON_INPUT;
 
     work_regime_choice(&working_mode);
 
-    if (working_mode == 1) // handle input of coefficients
+    if (working_mode ==  HANDLE_INPUT)
     {
         bool out_check = false; //checks if a user wants to finish and enters 'q'
         printf("\nOk, let's do it! Just enter coefficients through a space, also write down zeros, 3 numbers in common.\n"
@@ -49,50 +48,88 @@ int main(void)
 
         while(!out_check)
         {
+            struct SquareEq EqParametrs = {};
+
             enter_coefficients_manually(&out_check, &EqParametrs);
+
             if (out_check)
             {
                 break;
             }
+
             printf("check result: a = %lg, b = %lg, c = %lg.\n", EqParametrs.a, EqParametrs.b, EqParametrs.c);
+
             solve_quadratic_equation(&EqParametrs);
+
             result_output(&EqParametrs);
         }
+
         printf("It was nice to see you here! Come back!\n");
     }
 
-    else // UNIT TEST
+    else if (working_mode == UNIT_TEST_INPUT)
     {
+        struct SquareEq EqParametrs = {};
+
         Unit_Test(&EqParametrs);
     }
+
+
+    else if (working_mode == FILE_INPUT)
+    {
+        struct SquareEq EqParametrs = {};
+
+        File_Input(&EqParametrs);
+    }
+
 
     return 0;
 }
 
-//enum
-
-void work_regime_choice(int * working_mode)
+void work_regime_choice(enum Working_Mode * working_mode)
 {
     assert(working_mode != nullptr);
 
     int status = 0; //varieble shows the result of scanf, 1 == success
-    printf("Hello, stranger! Do you wanna solve a quadratic "
-           "equation?\nNevertheless, Poltorashka said we gonna do it.\n\n"
-           "So, I wanna you to enter three coefficients: a, b and c respectively\n"
-           "Or you can choose an input from the file (in developing)\n\n"
-           "Enter 1, if you wanna enter coefficients by yourself\n"
-           "or 2, if you wanna chose the input from the file: ");
+    int mode = 0;
+    printf("Hello, stranger! Do you wanna solve a quadratic equation?\n"
+           "Nevertheless, Poltorashka said we gonna do it.\n\n"
 
-    while ((status = scanf("%d", working_mode)) != 1 or (*working_mode < 1 or *working_mode > 2)) //Entering the working mode with errors processing
+           "So, I wanna you to enter three coefficients mannually: a, b and c respectively\n"
+           "You also can choose automatic test of the programm with built - in coeffs\n"
+           "Or you can choose an input from the file (in developing)\n\n"
+
+           "Enter 1, if you wanna enter coefficients by yourself\n"
+           "      2, if you wanna choose the automatic test \n"
+           "      3, if you wanna choode the automatic test with file input\n>");
+
+    while ((status = scanf("%d", &mode)) != 1 or (mode < 1 or mode > 3)) //Entering the working mode with errors processing
     {
         if (status != 1)
         {
-            while (getchar() != '\n') //buffer cleaning
-            {
-                continue;
-            }
+        buffer_cleaning();
         }
+
         printf("Please, enter an integer number, 1 or 2: ");
+    }
+
+    switch (mode)
+    {
+    case 1:
+        *working_mode = HANDLE_INPUT;
+        break;
+
+    case 2:
+        *working_mode = UNIT_TEST_INPUT;
+        break;
+
+    case 3:
+        *working_mode = FILE_INPUT;
+        break;
+
+    default:
+        printf("OOPS :(  Something went wrong and work regime choice has broken");
+        break;
     }
 
 }
@@ -109,10 +146,8 @@ void enter_coefficients_manually(bool * out_check, struct SquareEq * EqParametrs
             break;
         }
 
-        while (getchar() != '\n') //buffer cleaning
-        {
-            continue;
-        }
+        buffer_cleaning();
+
         printf("Enter 3 numbers, like that: \"1 2 3\", or enter 'q' to finish\n>");
     }
 }
@@ -132,14 +167,11 @@ void solve_quadratic_equation(struct SquareEq * EqParametrs)
     if (dicriminant < 0)
     {
         EqParametrs->nRoots = ZERO_ROOTS;
-        EqParametrs->x1 = 0.0;
-        EqParametrs->x2 = 0.0;
     }
 
     if (are_equal(dicriminant, 0.0))
     {
         EqParametrs->x1 = (-(EqParametrs->b))/(2*(EqParametrs->a));
-        EqParametrs->x2 = 0.0;
         if (are_equal(EqParametrs->x1, 0.0)) EqParametrs->x1 = 0.0;
         EqParametrs->nRoots = ONE_ROOT;
     }
@@ -163,19 +195,14 @@ void solve_linear_equation(struct SquareEq * EqParametrs)
         if (are_equal(EqParametrs->c, 0.0))
         {
             EqParametrs->nRoots = INF_ROOTS;
-            EqParametrs->x1 = 0.1;
-            EqParametrs->x2 = 0.1;
             return;
         }
 
         EqParametrs->nRoots = ZERO_ROOTS;
-        EqParametrs->x1 = 0.0;
-        EqParametrs->x2 = 0.0;
         return;
     }
 
     EqParametrs->x1 = (-(EqParametrs->c))/(EqParametrs->b);
-    EqParametrs->x2 = 0.0;
     EqParametrs->nRoots = ONE_ROOT;
 }
 
@@ -189,18 +216,21 @@ void result_output(struct SquareEq * EqParametrs)
 
     switch (EqParametrs->nRoots)
     {
-    case ZERO_ROOTS:
-        printf("the equasion has 0 solutions\n\n>");
-        break;
-    case ONE_ROOT:
-        printf("The equasion has 1 solution. x = %lg\n\n>", EqParametrs->x1);
-        break;
-    case TWO_ROOTS:
-        printf("The equasion has 2 solutions. x1 = %lg, x2 = %lg\n\n>", EqParametrs->x1, EqParametrs->x2);
-        break;
-    case INF_ROOTS:
-        printf("The equasion has an infinity of solutions\n\n>");
-        break;
+        case ZERO_ROOTS:
+            printf("the equasion has 0 solutions\n\n>");
+            break;
+
+        case ONE_ROOT:
+            printf("The equasion has 1 solution. x = %lg\n\n>", EqParametrs->x1);
+            break;
+
+        case TWO_ROOTS:
+            printf("The equasion has 2 solutions. x1 = %lg, x2 = %lg\n\n>", EqParametrs->x1, EqParametrs->x2);
+            break;
+
+        case INF_ROOTS:
+            printf("The equasion has an infinity of solutions\n\n>");
+            break;
     }
 }
 
@@ -209,38 +239,83 @@ bool are_equal(double a, double b)
     return (fabs(a - b) <= eps);
 }
 
+void buffer_cleaning()
+{
+    while (getchar() != '\n')
+    {
+        continue;
+    }
+}
+
 void Unit_Test(struct SquareEq * EqParametrs)
 {
-    double Tests[10][6] =
+    double Tests[TESTS_AMOUNT][6] =
     {
-        {2, -7, 3, 2, 0.5, 3},   // a, b, c, Expected_nRoots, Expected_x1, Expected_x2
-        {0, 0, 0, 3, 0.1, 0.1},
-        {4, -44, 121, 1, 5.5, 0.0},
-        {0, 16, 56, 1, -3.5, 0.0},
-        {0, 12, 0, 1, 0, 0.0},
-        {3, -5, -8, 2, -1, (8.0/3.0)},
-        {1, 0, 0, 1, 0, 0.0},
-        {1, 0, -81, 2, -9, 9},
-        {1, -1, -40200, 2, -200, 201},
-        {1, 33, -34, 2, -34, 1}
+        {2,  -7,      3, 2,      0.5,         3},   // a, b, c, Expected_nRoots, Expected_x1, Expected_x2
+        {0,   0,      0, 3,      0.0,       0.0},
+        {4, -44,    121, 1,      5.5,       0.0},
+        {0,  16,     56, 1,     -3.5,       0.0},
+        {0,  12,      0, 1,        0,       0.0},
+        {3,  -5,     -8, 2,       -1, (8.0/3.0)},
+        {1,   0,      0, 1,        0,       0.0},
+        {1,   0,    -81, 2,       -9,         9},
+        {1,  -1, -40200, 2,     -200,       201},
+        {1,  33,    -34, 2,      -34,         1},
+        {1,   0,      1, 0,      0.0,       0.0}
     };
 
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < TESTS_AMOUNT; i++)
     {
+        (*EqParametrs) = {};
         EqParametrs->a = Tests[i][0];
         EqParametrs->b = Tests[i][1];
         EqParametrs->c = Tests[i][2];
+
         solve_quadratic_equation(EqParametrs);
+
         int Expected_nRoots = Tests[i][3];
-        double Expected_x1 = Tests[i][4];
-        double Expected_x2 = Tests[i][5];
+        double Expected_x1  = Tests[i][4];
+        double Expected_x2  = Tests[i][5];
+
         Checks_values(i, EqParametrs, Expected_nRoots, Expected_x1, Expected_x2);
     }
 }
 
+void File_Input(struct SquareEq * EqParametrs)
+{
+    FILE *file;
+    char file_name[] = "file input.txt";
+
+    if ((file = fopen(file_name, "r")) == NULL)
+    {
+        printf("OoOps :( Please, create file \"file input.txt\" with tests and try again");
+        return;
+    }
+
+    int Expected_nRoots = 0;
+    double Expected_x1 = 0.0;
+    double Expected_x2 = 0.0;
+
+    int i = 0;
+
+    while ((fscanf(file, "%lg, %lg, %lg, %d, %lg, %lg", &(EqParametrs->a), &(EqParametrs->b), &(EqParametrs->c),
+                &Expected_nRoots, &Expected_x1, &Expected_x2)) == 6)
+    {
+        solve_quadratic_equation(EqParametrs);
+
+        Checks_values(i, EqParametrs, Expected_nRoots, Expected_x1, Expected_x2);
+
+        (*EqParametrs) = {};
+
+        i++;
+    }
+    fclose(file);
+}
+
 void Checks_values(int i, struct SquareEq * EqParametrs, int Expected_nRoots, double Expected_x1, double Expected_x2)
 {
-    if ((EqParametrs->nRoots != Expected_nRoots) || !are_equal(EqParametrs->x1, Expected_x1) || !are_equal(EqParametrs->x2, Expected_x2))
+    if ((EqParametrs->nRoots != Expected_nRoots) || !are_equal(EqParametrs->x1, Expected_x1) ||
+                                                    !are_equal(EqParametrs->x2, Expected_x2))
     {
         printf("\nFAILED Test %d. \n"
                 "Expected: nRoots = %d, x1 = %10lg, x2 = %10lg\n"
@@ -248,6 +323,9 @@ void Checks_values(int i, struct SquareEq * EqParametrs, int Expected_nRoots, do
                 EqParametrs->nRoots, EqParametrs->x1, EqParametrs->x2);
         return;
     }
+
     printf("success Test %d\n", i+1);
 }
+
+
 
--- main.h
+++ main.h
@@ -1,67 +1,122 @@
+enum Working_Mode
+{
+    /**
+     * @brief Neede to choose the working mode of the programm
+     * 
+     * HANDLE_INPUT    needed to enter coefficients from keyboard
+     * UNIT_TEST_INPUT needed to enter coefficients from the massive in the programm
+     * FILE_INPUT      needed to enter coefficients from the file
+     * NON_INPUT       default value untill user changes it
+     */
+
+    HANDLE_INPUT,
+    UNIT_TEST_INPUT,
+    FILE_INPUT,
+    NON_INPUT
+};
+
 /**
  * @brief function to get coefficients by manual input
- * 
+ *
  * Gets three numbers and write them to the parametrs of the equasion
- * 
+ *
  * @param out_check checks if a user enters 'q' and finish entering after that
  * @param EqParametrs has all parametrs of current equasion
- * 
+ *
  * @see struct SquareEq
- * 
+ *
 */
+
+
 void enter_coefficients_manually(bool * out_check, struct SquareEq * EqParametrs);
 
 /**
  * @brief function to solve quadratic equasion
- * 
+ *
  * Function calls function of linear equasion solving if a == 0 and solves quadratic equasion
  * if a != 0 with help of discriminant
- * 
+ *
  * @param EqParametrs has all parametrs of current equasion
- * 
+ *
  * @see struct SquareEq
- * 
+ *
 */
 void solve_quadratic_equation(struct SquareEq * EqParametrs);
 
 /**
  * @brief function solves linear equasion
- * 
+ *
  * Is called from solve_quadratic_equasion if a-coeff == 0
- * 
+ *
  * @param EqParametrs has all parametrs of current equasion
- * 
+ *
  * @see struct SquareEq
 */
 void solve_linear_equation(struct SquareEq * EqParametrs);
 
 /**
  * @brief puts the resulf of solving the quadratic equasion after manual input
- * 
+ *
  * @param EqParametrs has all parametrs of current equasion
- * 
+ *
  * @see struct SquareEq
 */
 void result_output(struct SquareEq * EqParametrs);
 
 /**
  * @brief function to choose the working mode
- * 
+ *
  * Takes 1 or 2 in handle input and chooses regime of work
- * 
+ *
  * @param *working_mode gets the type of work from the user, 1 is manual input, 2 is file input
 */
-void work_regime_choice(int * working_mode);
+void work_regime_choice(enum Working_Mode * working_mode);
 
 /**
  * @brief function checks an approximate equality of double numbers
- * 
+ *
  * @param a is the first number
  * @param b is the second number
- * 
+ *
 */
 bool are_equal(double a, double b);
 
+
+/**
+ * @brief Checks the algoritm of quadratic equasion solving 
+ * 
+ * Has a massive of massives with 3 coefficients (a, b, c), 
+ * expected amount of roots, expected answeres.
+ * Calls the function of quadratic equasion solving 
+ * and the function of comparison of received answeres and expected ones.
+ * 
+ * @param EqParametrs has all parametrs of current equasion
+ * 
+ * @see struct SquareEq
+ */
 void Unit_Test(struct SquareEq * EqParametrs);
 
+
+/**
+ * @brief Compares received answers with expected
+ * 
+ * @param i shows number of test
+ * @param EqParametrs has all parametrs of current equasion
+ * @param Expected_nRoots 
+ * @param Expected_x1 
+ * @param Expected_x2 
+ * 
+ * @see struct SquareEq
+ */
 void Checks_values(int i, struct SquareEq * EqParametrs, int Expected_nRoots, double Expected_x1, double Expected_x2);
+
+
+void File_Input(struct SquareEq * EqParametrs);
+
+/**
+ * @brief Cleans the buffer after enter
+ *
+ * Function skips symbols untill '\n'
+ *
+ */
+void buffer_cleaning();
